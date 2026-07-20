5195515409ea72a4092a99003b429bcaafdf7426
itsha123 <itsha123@proton.me>
2026-07-20 15:49:43 -0500

    add get data endpoint, add progress text, make data's time start from 0

--- web/backend/getdata.php
+++ web/backend/getdata.php
@@ -5,16 +5,13 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $raw_input = file_get_contents("php://input");
+    $raw_input = file_get_contents('php://input');    
     $username = hash_hmac("sha256", json_decode($raw_input, true)["username"], file_get_contents("../../.env"));
-    $data = json_decode($raw_input, true)["data"];
     $the_mouse_data = json_decode(file_get_contents("../../data.json"), true);
 
-    if ($the_mouse_data[$username] != null) {
-        $the_mouse_data[$username][] = $data;
+    if (isset($the_mouse_data[$username])) {
+        echo json_encode($the_mouse_data[$username]);
     } else {
-        $the_mouse_data[$username] = [$data];
+        echo json_encode([]);
     }
-    file_put_contents("../../data.json", json_encode($the_mouse_data));
 }
-?>
