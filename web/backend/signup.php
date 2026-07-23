f198ce521498d309cadc28f6120d51d5bcf98c1d
itsha123 <itsha123@proton.me>
2026-07-23 11:03:27 -0500

    Update to not collect custom usernames and passwords, only random usernames

--- web/backend/signup.php
+++ web/backend/signup.php
@@ -7,11 +7,10 @@ error_reporting(E_ALL);
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $raw_input = file_get_contents('php://input');    
     $username = hash_hmac("sha256", json_decode($raw_input, true)["username"], file_get_contents("../.env"));
-    $password = password_hash(json_decode($raw_input, true)["password"], PASSWORD_DEFAULT);
     $users_data = json_decode(file_get_contents("../users.json"), true);
 
     if (!array_key_exists($username, $users_data)) {
-        $users_data[$username] = $password;
+        $users_data[$username] = "";
         echo "True";
     } else {
         echo "False";
